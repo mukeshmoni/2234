31e0eb5466b3d98754bfe9d5ad5a38d63a2714d6
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2024-12-19 15:49:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     API_HASH = "26100c77cee02e5e34b2bbee58440f86"
 
      #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
-    TOKEN = os.environ.get("TOKEN", "7643773535:AAF5sLZpd_Zyl0yK8wYqteLWCtNvWnQiLVU")
+    TOKEN = os.environ.get("TOKEN", "7573604459:AAGwUKl86kOEelpd7H7-oKJE942bRJHEn-I")
     MONGO_URL = "mongodb+srv://mukeshmonidb:mukeshmoni@cluster0.e27s9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     START_PIC = "https://telegra.ph/file/0eba143d65f9413f9ae04.jpg"
     SUDOERS = filters.user(["5444362033"])
