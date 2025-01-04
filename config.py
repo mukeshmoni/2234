814ed9f77caec9d1a59cd222ca2d8557038afe9d
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2025-01-04 16:18:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "20611365"
+API_ID = "20611365"
 API_HASH = "424e183d2cf8a7f2c8bafeeaab1b5c8c"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
 TOKEN = os.environ.get("TOKEN", "7573604459:AAGtbbye_5vZJb6Ezimlm-jJIQ2aogOQdEw")
