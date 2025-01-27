d42b1b8f996d7a391648187a23f85bd5336e29c4
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2025-01-27 17:07:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "20611365"
-    API_HASH = "424e183d2cf8a7f2c8bafeeaab1b5c8c"
+  API_ID   "24274375"
+  API_HASH    "9d88affc9ab571a6695e069cef5d363b"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = "7573604459:AAGtbbye_5vZJb6Ezimlm-jJIQ2aogOQdEw"
     MONGO_URL = "mongodb+srv://ravi:ravi12345@cluster0.hndinhj.mongodb.net/?retryWrites=true&w=majority"
