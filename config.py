666e20f78ddb77092f858ec1c659c9995ecf6713
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2025-01-28 08:28:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,8 @@ from pyrogram import filters
 import os
 
 class Config:
-  API_ID = "24274375"
-  API_HASH = "9d88affc9ab571a6695e069cef5d363b"
-    #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
+    API_ID = "24274375"
+    API_HASH = "9d88affc9ab571a6695e069cef5d363b"
     TOKEN = "7573604459:AAGtbbye_5vZJb6Ezimlm-jJIQ2aogOQdEw"
     MONGO_URL = "mongodb+srv://moni:moni@cluster0.cei6n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     START_PIC = "https://telegra.ph/file/0eba143d65f9413f9ae04.jpg"
