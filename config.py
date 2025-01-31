79be6561382dacb59f2ce3be0aebf12c17513471
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2025-01-31 12:12:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     API_ID = "24274375"
     API_HASH = "9d88affc9ab571a6695e069cef5d363b"
     TOKEN = "7573604459:AAGtbbye_5vZJb6Ezimlm-jJIQ2aogOQdEw"
-    MONGO_DB_URI = "mongodb+srv://architect04:architect04@cluster0.fylqb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_DB_URI = "mongodb+srv://monifradu:moniabifradu@cluster0.cei6n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     START_PIC = "https://telegra.ph/file/0eba143d65f9413f9ae04.jpg"
     SUDOERS = filters.user(["7290811275"])
